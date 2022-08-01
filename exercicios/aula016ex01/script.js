c06654394b7da689854cde2f7d11455e17f4b8f0
Lucas dos Santos Souza <108701949+LucasSouzaEngineer@users.noreply.github.com>
2022-08-01 20:36:10 -0300

    aula 016 ex 01 finalizado

--- exercicios/aula016ex01/script.js
+++ exercicios/aula016ex01/script.js
@@ -4,8 +4,6 @@ var estatistica = window.document.createElement('p')
 function adicionar(){
     var txtnum = window.document.getElementById('txtnum')
     var numero = Number(txtnum.value)
-    
-
     var lista = window.document.getElementById('lista')
     var itemlista = window.document.createElement('option')
 
@@ -17,21 +15,30 @@ function adicionar(){
         lista.appendChild(itemlista)
     }
     estatistica.innerHTML = null
+    txtnum.value = ''
+    txtnum.focus()
 }
 
 function finalizar(){
-    var res = window.document.getElementById('res')
-    var tamanho = listanum.length
-    var maior = Math.max.apply(null, listanum)
-    var menor = Math.min.apply(null, listanum)
-    var soma = 0
-    var media = 0
+    if(listanum.length == 0){
+        window.alert('Adicione valores para serem analisados')
+    }else{
+        var res = window.document.getElementById('res')
+        var tamanho = listanum.length
+        var maior = Math.max.apply(null, listanum)
+        var menor = Math.min.apply(null, listanum)
+        var soma = 0
 
-    for(var i = 0; i < tamanho; i++){
-        soma += listanum[i]
+        for(var i = 0; i < tamanho; i++){
+            soma += listanum[i]
+        }
+
+        estatistica.innerHTML += `<p>Array ${listanum}</p>`
+        estatistica.innerHTML += `<p>Ao todo, temos ${tamanho} números cadastrados</p>`
+        estatistica.innerHTML += `<p>O maior valor informado foi: ${maior}</p>`
+        estatistica.innerHTML += `<p>O menor valor informado foi: ${menor}</p>`
+        estatistica.innerHTML += `<p>Somando todos os valores temos: ${soma}</p>`
+        estatistica.innerHTML += `<p>A media dos valores digitados é: ${soma/listanum.length}</p>`
+        res.appendChild(estatistica)
     }
-    estatistica.innerHTML = `Array ${listanum}<br> tamanho: ${tamanho}<br> maior: ${maior}<br> menor: ${menor}<br> soma: ${soma}<br> media: ${soma/listanum.length}`
-    //var estatistica = window.createElement('p')
-    //estatistica.text = `Array digitado`
-    res.appendChild(estatistica)
 }
